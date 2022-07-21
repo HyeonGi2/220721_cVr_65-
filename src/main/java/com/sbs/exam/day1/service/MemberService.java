ab72b0816b9c82d9a2e917997987fc51bcc32290
HyeonGi2 <tmskc56@gmail.com>
2022-07-22 03:43:02 +0900

    cVr, 66강, 회원가입 시, 입력데이터 유효성 체크, 공백도 체크

--- src/main/java/com/sbs/exam/day1/service/MemberService.java
+++ src/main/java/com/sbs/exam/day1/service/MemberService.java
@@ -7,23 +7,25 @@ import com.sbs.exam.day1.vo.Member;
 
 @Service
 public class MemberService {
-	
+
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if ( oldMember != null) {
+
+		if (oldMember != null) {
 			return -1;
 		}
+
 		
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLastInsertId();
-		
+
 	}
 
 	private Member getMemberByLoginId(String loginId) {
